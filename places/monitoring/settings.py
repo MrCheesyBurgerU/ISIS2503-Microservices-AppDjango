15a05a2439d04d2b4f50ac5bdf02a082cd096061
MrCheesyBurgerU <n.rozo@uniandes.edu.co>
2024-05-18 20:35:52 -0500

    Modified App

--- places/monitoring/settings.py
+++ places/monitoring/settings.py
@@ -77,11 +77,11 @@ WSGI_APPLICATION = 'monitoring.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.environ.get("MEASUREMENTS_DB", "places_db"),
-        'USER': os.environ.get("MEASUREMENTS_DB_USER", "places_user"),
-        'PASSWORD': os.environ.get("MEASUREMENTS_DB_PASSWD", "isis2503"),
-        'HOST': os.environ.get("MEASUREMENTS_DB_HOST", "10.128.0.84"),
-        'PORT': os.environ.get("MEASUREMENTS_DB_PORT", "5432")
+        'NAME': os.environ.get("PLACES_DB", "places_db"),
+        'USER': os.environ.get("PLACES_DB_USER", "places_user"),
+        'PASSWORD': os.environ.get("PLACES_DB_PASSWD", "isis2503"),
+        'HOST': os.environ.get("PLACES_DB_HOST", "10.128.0.84"),
+        'PORT': os.environ.get("PLACES_DB_PORT", "5432")
     }
 }
 
